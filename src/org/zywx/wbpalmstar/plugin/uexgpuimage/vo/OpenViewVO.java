3cf66fcf55873e4083c58f71156ba1657e18e972
yltwust <510212753@qq.com>
2016-11-03 17:09:32 +0800

    init

---
+++ src/org/zywx/wbpalmstar/plugin/uexgpuimage/vo/OpenViewVO.java
@@ -0,0 +1,67 @@
+package org.zywx.wbpalmstar.plugin.uexgpuimage.vo;
+
+import java.io.Serializable;
+
+/**
+ * Created by ylt on 2016/10/31.
+ */
+
+public class OpenViewVO implements Serializable {
+
+    private int x=0;
+    private int y=0;
+    private int w;
+    private int h;
+
+    private String path;
+
+    private String type;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
